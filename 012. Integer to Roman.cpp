663d37ed545f18212864a2e8a7e429b2055211b7
Yilun Zhu <36528335+yilunzhu@users.noreply.github.com>
2018-03-20 18:30:26 -0700

    Update and rename 012. Integer to Roman to 012. Integer to Roman.cpp

--- 012. Integer to Roman.cpp
+++ 012. Integer to Roman.cpp
@@ -5,6 +5,6 @@ public:
         string b[] = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
         string c[] = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
         string d[] = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
-        return a[n/1000] + b[(n/100) % 10] + c[(n/10) % 10] + d[n % 10];
+        return a[n / 1000] + b[(n / 100) % 10] + c[(n / 10) % 10] + d[n % 10];
     }
 };
