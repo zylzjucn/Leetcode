6481cf7f7c18e7685ec4bd553be165ad495b21b5
Yilun Zhu <36528335+yilunzhu@users.noreply.github.com>
2018-03-20 18:18:35 -0700

    Update and rename 009. Palindrome Number to 009. Palindrome Number.cpp

--- 009. Palindrome Number.cpp
+++ 009. Palindrome Number.cpp
@@ -4,13 +4,12 @@ public:
         int r = 0;
         int t;
         int y = x;
-        while(x!=0)
-        {
-            t = x%10;
+        while(x != 0) {
+            t = x % 10;
             r = 10 * r + t;
-            x = x/10;
+            x = x / 10;
         }
-        return ((y == r)&& y>=0);
+        return ((y == r) && y>=0);
     }
 };
 
