589bcd2c3afaba4828fb5bd3bb8d397b0d583117
Yilun Zhu <36528335+yilunzhu@users.noreply.github.com>
2018-03-20 18:37:32 -0700

    Update 007. Reverse Integer.cpp

--- 007. Reverse Integer.cpp
+++ 007. Reverse Integer.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
-    int reverse(int x) 
-    {
+    int reverse(int x) {
         int result = 0;
         int temp;
         int tail;
-        while (x!=0) {
+        while (x != 0) {
             tail = x % 10;
             temp = 10 * result + tail;
             if ((temp - tail)/10 != result)
