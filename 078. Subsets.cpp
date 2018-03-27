b1ac7073fe1e505bcca4ca02873ad748898d8e88
Yilun Zhu <36528335+yilunzhu@users.noreply.github.com>
2018-03-26 18:08:46 -0700

    Update 078. Subsets.cpp

--- 078. Subsets.cpp
+++ 078. Subsets.cpp
@@ -7,10 +7,7 @@ public:
         for (int i = 0; i < n.size(); i++) {
             int len = res.size();
             for (int j = 0; j < len; j++) {
-                if (res[j].size() == 0)
-                    v.clear();
-                else
-                    v = res[j];
+                v = res[j];
                 v.push_back(n[i]);
                 res.push_back(v);
             }
