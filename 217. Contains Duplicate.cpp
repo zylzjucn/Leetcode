7dc499714d7c630661adfdc31999a4ee70b5478c
Yilun Zhu <36528335+yilunzhu@users.noreply.github.com>
2018-04-11 03:15:28 -0700

    Update 217. Contains Duplicate.cpp

--- 217. Contains Duplicate.cpp
+++ 217. Contains Duplicate.cpp
@@ -3,11 +3,10 @@ public:
     bool containsDuplicate(vector<int>& n) {
         if (n.size() == 0)
             return false;
-        set<int> s;
-        for (int i = 0; i < n.size(); ++i) {
-            if (s.find(n[i]) != s.end())
+        sort(n.begin(), n.end());
+        for (int i = 0; i < n.size() - 1; ++i) {
+            if (n[i] == n[i + 1])
                 return true;
-            s.insert(n[i]);
         }
         return false;
     }
