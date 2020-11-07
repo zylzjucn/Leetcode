8b21e2a719c169546e11f8250abfdb04c0d1bcb1
Yilun Zhu <zylzjucn@outlook.com>
2020-11-07 17:22:32 -0600

    041

--- 0~99/041. First Missing Positive.cpp
+++ 0~99/041. First Missing Positive.cpp
@@ -1,18 +1,12 @@
 class Solution {
 public:
-    int firstMissingPositive(vector<int>& n) {
-        int res;
-        set<int> s;
-        for (int i = 0; i < n.size(); i++) {
-            if (n[i] > 0)
-                s.insert(n[i]);
-        }
-        int i = 1;
-        for (set<int>::iterator it = s.begin(); it != s.end(); it++, i++) {
-            if (*it != i)
-                return i;
-        }
-        return i;
+    int firstMissingPositive(vector<int>& nums) {
+        for (int i = 0; i < nums.size(); i++)
+            while (nums[i] > 0 && nums[i] <= nums.size() && nums[i] != nums[nums[i] - 1])
+                swap(nums[i], nums[nums[i] - 1]);
+        for (int i = 0; i < nums.size(); i++)
+            if (nums[i] - 1 != i)
+                return i + 1;
+        return nums.size() + 1;
     }
-}
-
+};
