358be6b03e81daad96c21d6cd195a3a9933d950c
Yilun Zhu <36528335+zylzjucn@users.noreply.github.com>
2025-02-06 23:32:20 -0800

    Update 296. Best Meeting Point.cpp

--- 200~299/296. Best Meeting Point.cpp
+++ 200~299/296. Best Meeting Point.cpp
@@ -1,36 +1,41 @@
 class Solution {
 public:
-    int minTotalDistance(vector<vector<int>>& m) {
-        vector<int> x(m[0].size(), 0);
-        vector<int> y(m.size(), 0);
-        for (int i = 0; i < m.size(); i++)
-            for (int j = 0; j < m[0].size(); j++) {
-                x[j] += m[i][j];
-                y[i] += m[i][j];
-            }
-        return get_distance(x) + get_distance(y);
-    }
-    
-private:
-    int get_distance(vector<int> v) {
-        int i = 0;
-        int j = v.size() - 1;
-        int left = v[0];
-        int right = v.back();
-        int stepl = 0;
-        int stepr = 0;
-        while (i < j) {
-            if (left <= right) {
-                stepl += left;
-                i++;
-                left += v[i];
-            }
-            else if (left > right) {
-                stepr += right;
-                j--;
-                right += v[j];
+    int minTotalDistance(vector<vector<int>>& grid) {
+        vector<int> horizontal(grid[0].size(), 0);
+        vector<int> vertical(grid.size(), 0);
+        for (int i = 0; i < grid.size(); i++) {
+            for (int j = 0; j < grid[0].size(); j++) {
+                vertical[i] += grid[i][j];
+                horizontal[j] += grid[i][j];
             }
         }
-        return stepl + stepr;
+        int res = 0;
+        int left = 0, right = horizontal.size() - 1;
+        int left_count = horizontal[0], right_count = horizontal.back();
+        while (left < right) {
+            if (left_count < right_count) {
+                left++;
+                res += left_count;
+                left_count += horizontal[left];
+            } else {
+                right--;
+                res += right_count;
+                right_count += horizontal[right];
+            }
+        }
+        int up = 0, bottom = vertical.size() - 1;
+        int up_count = vertical[0], bottom_count = vertical.back();
+        while (up < bottom) {
+            if (up_count < bottom_count) {
+                up++;
+                res += up_count;
+                up_count += vertical[up];
+            } else {
+                bottom--;
+                res += bottom_count;
+                bottom_count += vertical[bottom];
+            }
+        }
+        return res;
     }
 };
