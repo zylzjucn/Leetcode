0563540ee6182f8aac5c14e0196620c6ea215fb3
Yilun Zhu <36528335+zylzjucn@users.noreply.github.com>
2025-02-07 01:12:33 -0800

    Update 317. Shortest Distance from All Buildings.cpp

--- 300~399/317. Shortest Distance from All Buildings.cpp
+++ 300~399/317. Shortest Distance from All Buildings.cpp
@@ -2,43 +2,46 @@ class Solution {
 public:
     int shortestDistance(vector<vector<int>>& grid) {
         int res = INT_MAX;
-        vector<vector<int>> distances(grid.size(), vector<int>(grid[0].size(), 0));
-        vector<vector<int>> directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-        int id_building = 0;
+        vector reach(grid.size(), vector<int>(grid[0].size(), 0));
+        vector distances(grid.size(), vector<int>(grid[0].size(), 0));
+        int building = 0;
+        vector<pair<int, int>> directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
         for (int i = 0; i < grid.size(); i++) {
             for (int j = 0; j < grid[0].size(); j++) {
                 if (grid[i][j] == 1) {
-                    id_building--;
+                    building++;
+                    vector visited(grid.size(), vector<bool>(grid[0].size(), false));
                     queue<pair<int, int>> q;
-                    q.push(make_pair(i, j));
+                    q.push({i, j});
+                    visited[i][j] = true;
                     int distance = 0;
-                    q.push(make_pair(-1, -1));
-                    while (q.size() > 1) {
-                        int row = q.front().first;
-                        int col = q.front().second;
-                        q.pop();
-                        if (row == -1) {
-                            distance++;
-                            q.push(make_pair(-1, -1));
-                            continue;
-                        }
-                        distances[row][col] += distance;
-                        for (const auto& direction : directions) {
-                            int row_new = row + direction[0];
-                            int col_new = col + direction[1];
-                            if (row_new >= 0 && row_new < grid.size() && col_new >= 0 && col_new < grid[0].size() && grid[row_new][col_new] == id_building + 1) {
-                                q.push(make_pair(row_new, col_new));
-                                grid[row_new][col_new] = id_building;
+                    while (!q.empty()) {
+                        int len = q.size();
+                        for (int k = 0; k < len; k++) {
+                            auto [x, y] = q.front();
+                            q.pop();
+                            distances[x][y] += distance;
+                            reach[x][y]++;
+                            for (const auto& direction : directions) {
+                                int new_x = x + direction.first;
+                                int new_y = y + direction.second;
+                                if (new_x < 0 || new_x >= grid.size() || new_y < 0 || new_y >= grid[0].size() || visited[new_x][new_y] || grid[new_x][new_y] > 0) {
+                                    continue;
+                                }
+                                q.push({new_x, new_y});
+                                visited[new_x][new_y] = true;
                             }
                         }
+                        distance++;
                     }
                 }
             }
         }
         for (int i = 0; i < grid.size(); i++) {
             for (int j = 0; j < grid[0].size(); j++) {
-                if (grid[i][j] == id_building)
+                if (grid[i][j] == 0 && building == reach[i][j]) {
                     res = min(res, distances[i][j]);
+                }
             }
         }
         return res == INT_MAX ? -1 : res;
