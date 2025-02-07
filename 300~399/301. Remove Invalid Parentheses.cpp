fb5dfb5146847b90713ea5bf58c1f3ed609d806f
Yilun Zhu <36528335+zylzjucn@users.noreply.github.com>
2025-02-06 21:41:40 -0800

    Update 301. Remove Invalid Parentheses.cpp

--- 300~399/301. Remove Invalid Parentheses.cpp
+++ 300~399/301. Remove Invalid Parentheses.cpp
@@ -5,24 +5,30 @@ public:
         remove(res, s, '(', ')', 0, 0);
         return res;
     }
-    void remove(vector<string> &res, string s, char c1, char c2, int lasti, int lastj) {
+
+    void remove(vector<string>& res, string s, char c1, char c2, int last_i, int last_j) {
         int count = 0;
-        for (int i = lasti; i < s.length(); i++) {
-            if (s[i] == c1)
+        for (int i = last_i; i < s.length(); i++) {
+            if (s[i] == c1) {
                 count++;
-            else if (s[i] == c2)
+            } else if (s[i] == c2) {
                 count--;
-            if (count >= 0)
+            }
+            if (count >= 0) {
                 continue;
-            for (int j = lastj; j <= i; j++)
-                if (s[j] == c2 && s[j - 1] != c2)
+            }
+            for (int j = last_j; j <= i; j++) {
+                if (s[j] == c2 && (j == 0 || (j > 0 && s[j - 1] != c2))) {
                     remove(res, s.substr(0, j) + s.substr(j + 1), c1, c2, i, j);
+                }
+            }
             return;
         }
         reverse(s.begin(), s.end());
-        if (c1 == '(')
+        if (c1 == '(') {
             remove(res, s, ')', '(', 0, 0);
-        else
+        } else {
             res.push_back(s);
+        }
     }
-};  
+};
