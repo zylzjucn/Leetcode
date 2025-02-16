e53518ad795d4c60b7c0329aa36d5c68c6367332
Yilun Zhu <36528335+zylzjucn@users.noreply.github.com>
2025-02-15 20:17:48 -0800

    Update 329. Longest Increasing Path in a Matrix.cpp

--- 300~399/329. Longest Increasing Path in a Matrix.cpp
+++ 300~399/329. Longest Increasing Path in a Matrix.cpp
@@ -37,3 +37,41 @@ public:
         return true;
     }
 };
+
+class Solution {
+public:
+    int longestIncreasingPath(vector<vector<int>>& matrix) {
+        auto comp = [&matrix](const pair<int, int>& a, const pair<int, int>& b) {
+            return matrix[a.first][a.second] > matrix[b.first][b.second];
+        };
+        priority_queue<pair<int, int>, vector<pair<int, int>>, decltype(comp)> pq(comp);
+        for (int i = 0; i < matrix.size(); i ++) {
+            for (int j = 0; j < matrix[0].size(); j++) {
+                pq.push(make_pair(i, j));
+            }
+        }
+        vector m(matrix.size(), vector(matrix[0].size(), 1));
+        vector<pair<int, int>> directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        int res = 0;
+        while (!pq.empty()) {
+            int i = pq.top().first;
+            int j = pq.top().second;
+            pq.pop();
+            for (const auto& direction : directions) {
+                int new_i = i + direction.first;
+                int new_j = j + direction.second;
+                if (new_i >= 0 && new_i < matrix.size() && new_j >= 0 && new_j < matrix[0].size()) {
+                    if (matrix[i][j] < matrix[new_i][new_j]) {
+                        m[new_i][new_j] = max(m[new_i][new_j], m[i][j] + 1);
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < matrix.size(); i ++) {
+            for (int j = 0; j < matrix[0].size(); j++) {
+                res = max(res, m[i][j]);
+            }
+        }
+        return res;
+    }
+};
