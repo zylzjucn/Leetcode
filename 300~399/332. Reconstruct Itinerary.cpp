b1e28ede4db1d7e48d21605d0fc35e86c8f0e205
Yilun Zhu <36528335+zylzjucn@users.noreply.github.com>
2025-02-16 14:21:57 -0800

    Update 332. Reconstruct Itinerary.cpp

--- 300~399/332. Reconstruct Itinerary.cpp
+++ 300~399/332. Reconstruct Itinerary.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     vector<string> findItinerary(vector<vector<string>>& tickets) {
-        vector<string> res;
         unordered_map<string, multiset<string>> m;
-        for (const auto& v : tickets)
-            m[v[0]].insert(v[1]);
-        visit(res, m, "JFK");
+        for (const auto& ticket : tickets) {
+            m[ticket[0]].insert(ticket[1]);
+        }
+        vector<string> res;
+        buildItinerary(m, res, "JFK");
         return vector<string>(res.rbegin(), res.rend());
     }
-    
-    void visit(vector<string>& res, unordered_map<string, multiset<string>>& m, string port) {
-        while (m[port].size()) {
+
+    void buildItinerary(unordered_map<string, multiset<string>>& m, vector<string>& res, string port) {
+        while (!m[port].empty()) {
             string next = *m[port].begin();
             m[port].erase(m[port].begin());
-            visit(res, m, next);
-            // m[port].insert(next);
+            buildItinerary(m, res, next);
         }
         res.push_back(port);
     }
