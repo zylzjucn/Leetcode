5a17a49d9bd20ba274c00ccaaf3e8b7ff4214d15
Yilun Zhu <36528335+zylzjucn@users.noreply.github.com>
2025-02-19 21:47:29 -0800

    Update 031. Next Permutation.cpp

--- 0~99/031. Next Permutation.cpp
+++ 0~99/031. Next Permutation.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
-    void nextPermutation(vector<int>& n) {
-        int i = n.size() - 2;
-        for (; i >= 0 && n[i] >= n[i + 1]; i--);
+    void nextPermutation(vector<int>& nums) {
+        int i = nums.size() - 2;
+        for (; i >= 0 && nums[i] >= nums[i + 1]; i--);
         int j = i + 1;
-        if (i != -1) {
-            for (; n[j] > n[i] && j < n.size(); j++);
+        if (i >= 0) {
+            for (; j < nums.size() && nums[j] > nums[i]; j++);
             j--;
-            swap(n[i], n[j]);
+            swap(nums[i], nums[j]);
         }
-        j = n.size() - 1;
-        i++;
-        for (; i < j; swap(n[i++], n[j--]));
+        reverse(nums.begin() + i + 1, nums.end());
     }
 };
