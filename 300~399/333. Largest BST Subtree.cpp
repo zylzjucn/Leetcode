9591be7dc29dc5422ab38cf03839b64514083d24
Yilun Zhu <36528335+zylzjucn@users.noreply.github.com>
2025-02-23 15:13:26 -0800

    Update 333. Largest BST Subtree.cpp

--- 300~399/333. Largest BST Subtree.cpp
+++ 300~399/333. Largest BST Subtree.cpp
@@ -4,36 +4,43 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+struct Data {
+    int min_val;
+    int max_val;
+    bool is_bst = false;
+    int count;
+    Data(int min_val, int max_val, bool is_bst, int count) : min_val(min_val), max_val(max_val), is_bst(is_bst), count(count) {}
+};
+
 class Solution {
 public:
     int largestBSTSubtree(TreeNode* root) {
         int res = 0;
-        isBST(root, res);
+        largestBSTSubtree(root, res);
         return res;
     }
-private:
-    int isBST(TreeNode* root, int &res) {
-        if (!root)
-            return 0;
-        int left = isBST(root->left, res);
-        int right = isBST(root->right, res);
-        if (left >= 0 && right >= 0 && (!root->left || root->val > treemax(root->left)) && (!root->right || root->val < treemin(root->right))) {
-            res = max(res, left + right + 1);
-            return left + right + 1;
+
+    Data* largestBSTSubtree(TreeNode* root, int& res) {
+        if (!root) {
+            Data* data = new Data(INT_MAX, INT_MIN, true, 0);
+            return data;
         }
-        return -1;
-    }
-    int treemax(TreeNode* root) {
-        while (root->right)
-            root = root->right;
-        return root->val;
-    }
-    int treemin(TreeNode* root) {
-        while (root->left)
-            root = root->left;
-        return root->val;
+        Data* data_left = largestBSTSubtree(root->left, res);
+        Data* data_right = largestBSTSubtree(root->right, res);
+        Data* data = new Data(INT_MAX, INT_MIN, false, 0);
+        if (data_left->max_val < root->val && data_right->min_val > root->val && data_left->is_bst && data_right->is_bst) {
+            data->min_val = min(data_left->min_val, root->val);
+            data->max_val = max(data_right->max_val, root->val);
+            data->is_bst = true;
+            data->count = data_left->count + data_right->count + 1;
+            res = max(res, data->count);
+        }
+        return data;
     }
 };
