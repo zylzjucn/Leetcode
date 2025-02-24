353ed81109611278b06f5510b390e15b4f62cde2
Yilun Zhu <36528335+zylzjucn@users.noreply.github.com>
2025-02-23 17:13:13 -0800

    Update 108. Convert Sorted Array to Binary Search Tree.cpp

--- 100~199/108. Convert Sorted Array to Binary Search Tree.cpp
+++ 100~199/108. Convert Sorted Array to Binary Search Tree.cpp
@@ -4,22 +4,23 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    TreeNode* sortedArrayToBST(vector<int>& n) {
-        if (n.size() == 0)
-            return NULL; 
-        if (n.size() == 1)
-            return new TreeNode(n[0]);
-        int m = n.size() / 2;
-        TreeNode* root = new TreeNode(n[m]);
-        vector<int> l(n.begin(), n.begin()+m);
-        vector<int> r(n.begin() + m + 1, n.end());
-        root->left = sortedArrayToBST(l);
-        root->right = sortedArrayToBST(r);
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        return sortedArrayToBST(nums, 0, nums.size() - 1);
+    }
+
+    TreeNode* sortedArrayToBST(vector<int>& nums, int left, int right) {
+        if (left > right) {
+            return nullptr;
+        }
+        int mid = (left + right) / 2;
+        TreeNode* root = new TreeNode(nums[mid], sortedArrayToBST(nums, left, mid - 1), sortedArrayToBST(nums, mid + 1, right));
         return root;
     }
 };
