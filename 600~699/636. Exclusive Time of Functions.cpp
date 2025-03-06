2c15b77fa8a3a149ef4cf2bcf8be6b48dfe5b397
Yilun Zhu <36528335+zylzjucn@users.noreply.github.com>
2025-03-05 16:39:09 -0800

    Update 636. Exclusive Time of Functions.cpp

--- 600~699/636. Exclusive Time of Functions.cpp
+++ 600~699/636. Exclusive Time of Functions.cpp
@@ -1,55 +1,29 @@
 class Solution {
 public:
-    vector<int> exclusiveTime(int n, vector<string>& logs_string) {
-        vector<int> res(n, 0);
-        vector<vector<int>> logs = transform(logs_string);
+    vector<int> exclusiveTime(int n, vector<string>& logs) {
+        vector<int> v(n, 0);
         stack<int> s;
-        int time = 0;
-        for (const auto log : logs) {
-            if (log[1] == 1) {
-                if (!s.empty()) {
-                    int id = s.top();
-                    res[id] += log[2] - time;
-                }
-                time = log[2];
-                s.push(log[0]);
+        int last_timestamp = 0;
+        for (const auto& str : logs) {
+            int i = 0;
+            int j = 0;
+            for (; str[j] != ':'; j++);
+            int function = stoi(str.substr(i, j));
+            i = j + 1;
+            j = i;
+            for (; str[j] != ':'; j++);
+            bool is_start = str[i] == 's';
+            int timestamp = stoi(str.substr(j + 1)) + (is_start ? 0 : 1);
+            if (!s.empty()) {
+                v[s.top()] += timestamp - last_timestamp;
             }
-            else {
-                int id = s.top();
-                res[id] += log[2] - time + 1;
-                time = log[2] + 1;
+            if (is_start) {
+                s.push(function);
+            } else {
                 s.pop();
             }
+            last_timestamp = timestamp;
         }
-        return res;
-    }
-    
-    vector<vector<int>> transform(vector<string>& logs) {
-        vector<vector<int>> res;
-        for (const auto& log : logs) {
-            vector<int> v;
-            int start = 0;
-            int end  = 0;
-            while (end < log.length()) {
-                if (log[end] != ':')
-                    end++;
-                else {
-                    if (v.empty()) {
-                        v.push_back(stoi(log.substr(start, end)));
-                    }
-                    else {
-                        if (log[start] == 's')
-                            v.push_back(1);
-                        else
-                            v.push_back(-1);
-                    }
-                    start = end + 1;
-                    end++;
-                }
-            }
-            v.push_back(stoi(log.substr(start)));
-            res.push_back(v);
-        }
-        return res;
+        return v;
     }
 };
