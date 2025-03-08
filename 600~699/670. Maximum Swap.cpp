c35ddcaeafb9f5061a00cdfaed75d784209ea339
Yilun Zhu <36528335+zylzjucn@users.noreply.github.com>
2025-03-07 22:52:43 -0800

    Update 670. Maximum Swap.cpp

--- 600~699/670. Maximum Swap.cpp
+++ 600~699/670. Maximum Swap.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
     int maximumSwap(int num) {
-        string s = to_string(num);
-        int maxindex = s.length() - 1;
-        int minindex = s.length() - 1;
-        for (int i = s.length() - 2; i >= 0; i--) {
-            if (s[i] < s[maxindex])
-                minindex = i;
-            else if (s[i] > s[maxindex])
-                maxindex = i;
-        }
-        if (minindex >= maxindex) {
-            maxindex = minindex;
-            for (int i = minindex + 1; i < s.length(); i++) {
-                if (s[i] >= s[maxindex])
-                    maxindex = i;
+        string str = to_string(num);
+        int small_index = str.length();
+        int big_index_found = str.length();
+        int big_index = str.length() - 1;
+
+        for (int i = str.length() - 2; i >= 0; i--) {
+            if (str[i] < str[big_index]) {
+                small_index = i;
+                big_index_found = big_index;
+            } else if (str[i] > str[big_index]) {
+                big_index = i;
             }
         }
-        swap(s[minindex], s[maxindex]);
-        return stoi(s);
+
+        if (big_index_found == str.length()) {
+            return num;
+        }
+        swap(str[small_index], str[big_index_found]);
+        return stoi(str);
     }
 };
