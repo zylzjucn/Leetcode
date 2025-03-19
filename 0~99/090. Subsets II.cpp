033c1836a42395fb8951202a863cbbbd4386a045
Yilun Zhu <36528335+zylzjucn@users.noreply.github.com>
2025-03-18 21:12:52 -0700

    Update 090. Subsets II.cpp

--- 0~99/090. Subsets II.cpp
+++ 0~99/090. Subsets II.cpp
@@ -1,22 +1,17 @@
 class Solution {
 public:
-    vector<vector<int>> subsetsWithDup(vector<int>& n) {
-        vector<vector<int>> res;
-        vector<int> v;
-        res.push_back(v);
-        map<int, int> m;
-        for (int i = 0; i < n.size(); ++i) {
-            if (m.find(n[i]) == m.end())
-                m[n[i]] = 1;
-            else
-                m[n[i]]++;
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        unordered_map<int, int> m;
+        for (const auto& num : nums) {
+            m[num]++;
         }
-        for (map<int, int> :: iterator it = m.begin(); it != m.end(); ++it) {
-            int l = res.size();
-            for (int i = 0; i < l * it->second; ++i) {
-                v = res[i];
-                v.push_back(it->first);
-                res.push_back(v);
+        vector<vector<int>> res = {{}};
+        for (const auto& p : m) {
+            int len = res.size();
+            for (int i = 0; i < len * p.second; i++) {
+                vector<int> cur = res[i];
+                cur.push_back(p.first);
+                res.push_back(cur);
             }
         }
         return res;
